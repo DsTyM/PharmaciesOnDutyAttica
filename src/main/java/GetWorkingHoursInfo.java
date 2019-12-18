15ee01bf003a448724d39ca77809510b3f870586
DsTyM <stavrosyomafias@gmail.com>
2019-12-18 02:14:01 +0200

    on getting working hours info, ArrayList changed to HastMap

--- src/main/java/GetWorkingHoursInfo.java
+++ src/main/java/GetWorkingHoursInfo.java
@@ -3,6 +3,7 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.stream.IntStream;
 
 public class GetWorkingHoursInfo {
@@ -16,41 +17,30 @@ public class GetWorkingHoursInfo {
         Document jsoupdoc;
 
         // Working Hours Ids seem to be from 1 to 29 and from 32 to 40
-        var workingHoursIds = new ArrayList<String>();
+        var workingHoursIds = new ArrayList<Integer>();
         for (int i = 1; i <= 40; i++) {
             int finalI = i;
 
             // if i not in...
             if (IntStream.of(14, 15, 16, 17, 21, 22, 30, 31).noneMatch(x -> x == finalI)) {
-                workingHoursIds.add(Integer.toString(i));
+                workingHoursIds.add(i);
             }
         }
 
-        for (var id : workingHoursIds) {
+        var pageInfoById = new HashMap<Integer, String>();
 
+        for (var id : workingHoursIds) {
             url = tempUrl + id;
             jsoupdoc = Jsoup.connect(url).get();
-
-            var listOfPharmacyInfo = new ArrayList<String[]>();
 
             String pageInfo;
             var xPath = "html body center table tbody tr:eq(1) td table tbody tr:eq(1)";
             pageInfo = jsoupdoc.select(xPath).text();
             pageInfo = pageInfo.substring(pageInfo.indexOf(":") + 1).trim();
 
-            var tempArr = new String[2];
-        /*
-            1) Working Hours Id
-            2) Working Hours
-         */
-
-            tempArr[0] = id;
-            tempArr[1] = pageInfo;
-            listOfPharmacyInfo.add(tempArr);
-
-            for (var info : listOfPharmacyInfo) {
-                System.out.println(info[0] + ", " + info[1]);
-            }
+            pageInfoById.put(id, pageInfo);
         }
+
+        pageInfoById.forEach((id, pageInfo) -> System.out.println(id + ", " + pageInfo));
     }
 }
