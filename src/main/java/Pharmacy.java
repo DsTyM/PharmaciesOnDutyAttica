f5ac4f555b03308ccbb3fbc4f1b4d05f0058344e
DsTyM <stavrosyomafias@gmail.com>
2019-12-21 01:37:16 +0200

    creating setters and getters for Pharmacy class

--- src/main/java/Pharmacy.java
+++ src/main/java/Pharmacy.java
@@ -4,4 +4,44 @@ public class Pharmacy {
     private String address;
     private String region;
     private String phoneNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
