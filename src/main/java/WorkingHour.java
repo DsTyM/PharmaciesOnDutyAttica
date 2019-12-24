b8fee22a1c1786c0d57007300aca417d6beb9306
DsTyM <stavrosyomafias@gmail.com>
2019-12-24 02:39:37 +0200

    setters and getters created for WorkingHour class

--- src/main/java/WorkingHour.java
+++ src/main/java/WorkingHour.java
@@ -1,4 +1,20 @@
 public class WorkingHour {
     private int id;
     private String workingHourText;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWorkingHourText() {
+        return workingHourText;
+    }
+
+    public void setWorkingHourText(String workingHourText) {
+        this.workingHourText = workingHourText;
+    }
 }
