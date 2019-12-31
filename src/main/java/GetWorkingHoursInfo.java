23c5f65e4d46c11c0b05c033f218d75071468826
DsTyM <stavrosyomafias@gmail.com>
2019-12-31 17:05:18 +0200

    code refactored

--- src/main/java/GetWorkingHoursInfo.java
+++ src/main/java/GetWorkingHoursInfo.java
@@ -19,22 +19,19 @@ public class GetWorkingHoursInfo {
         String url;
         Document jsoupdoc;
 
-        // Working Hours Ids seem to be from 1 to 29 and from 32 to 40
-        var workingHoursIds = new ArrayList<Integer>();
-        for (int i = 1; i <= 40; i++) {
-            int finalI = i;
-
-            // if i not in...
-            if (IntStream.of(14, 15, 16, 17, 21, 22, 30, 31).noneMatch(x -> x == finalI)) {
-                workingHoursIds.add(i);
-            }
-        }
+        // Working Hours Ids seem to be from 1 to 40
+        var workingHoursIds = IntStream.range(1, 40).toArray();
 
         var listOfWorkingHours = new ArrayList<WorkingHour>();
 
         for (var id : workingHoursIds) {
             url = tempUrl + id;
-            jsoupdoc = Jsoup.connect(url).get();
+
+            try {
+                jsoupdoc = Jsoup.connect(url).get();
+            } catch (Exception e) {
+                continue;
+            }
 
             String pageInfo;
             var xPath = "html body center table tbody tr:eq(1) td table tbody tr:eq(1)";
