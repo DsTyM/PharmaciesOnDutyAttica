bd7ff0653a42c25bf7fcbb5d795db839d7f93c3b
DsTyM <stavrosyomafias@gmail.com>
2020-01-02 06:10:39 +0200

    code refactored

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,20 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        PrintAvailablePharmacies.printAvailablePharmacies();
+        // 1. Get all available PharmacyIds with WorkingHourIds
+        // An array list of HashMaps that will contain every <PharmacyId, WorkingHourId> would be
+        // a good implementation.
+
+        PrintAvailablePharmacies.printAvailablePharmacies();
+
+        // 2. Get all WorkingHours (instances)
+        // An array list of WorkingHour would be a good implementation.
 
 //        GetWorkingHoursInfo.getWorkingHoursInfo();
 
-//        GetPharmaciesInfo.getPharmaciesInfo();
+        // 3. Get all Pharmacies (instances)
+        // An array list of Pharmacy would be a good implementation.
 
-        GetAllAvailablePharmacyIds.getAllAvailablePharmacyIds();
+//        GetPharmaciesInfo.getPharmaciesInfo();
     }
 }
