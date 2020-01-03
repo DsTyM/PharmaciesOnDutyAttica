2348831f0603f17018648d90977bdd5ab58f341d
DsTyM <stavrosyomafias@gmail.com>
2020-01-04 01:15:07 +0200

    code commented to not be triggered again

--- src/main/java/com/dstym/pharmaciesondutyattica/controller/GetDataController.java
+++ src/main/java/com/dstym/pharmaciesondutyattica/controller/GetDataController.java
@@ -1,13 +1,9 @@
 package com.dstym.pharmaciesondutyattica.controller;
 
-import com.dstym.pharmaciesondutyattica.entity.AvailablePharmacy;
 import com.dstym.pharmaciesondutyattica.repository.AvailablePharmacyRepository;
 import com.dstym.pharmaciesondutyattica.repository.PharmacyRepository;
 import com.dstym.pharmaciesondutyattica.repository.WorkingHourRepository;
-import com.dstym.pharmaciesondutyattica.scraper.AvailablePharmacies;
-import com.dstym.pharmaciesondutyattica.util.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,36 +22,37 @@ public class GetDataController {
         this.availablePharmacyRepository = availablePharmacyRepository;
     }
 
-    @GetMapping("/get-available-pharmacies")
-    public String getAvailablePharmacies() {
-        var daysFromToday = 0;
-        var date = DateUtils.dateToString(DateUtils.getDateFromTodayPlusDays(daysFromToday));
-        var workingHoursIdByPharmacyId = AvailablePharmacies.getAvailablePharmacyIdsAndWorkingHourIds(daysFromToday);
-        AvailablePharmacy availablePharmacy;
-
-
-        var tempAvailablePharmacy =
-                (AvailablePharmacy) availablePharmacyRepository.getLastPulledVersion(date).toArray()[0];
-        var lastPulledVersion = tempAvailablePharmacy.getPulledVersion();
-
-        if (workingHoursIdByPharmacyId != null) {
-            for (var pair : workingHoursIdByPharmacyId.keySet()) {
-                int pharmacyId = pair;
-                int workingHourId = workingHoursIdByPharmacyId.get(pair);
-
-                availablePharmacy = new AvailablePharmacy();
-                availablePharmacy.setId(0);
-                availablePharmacy.setPharmacyId(pharmacyId);
-                availablePharmacy.setWorkingHourId(workingHourId);
-                availablePharmacy.setDate(date);
-                availablePharmacy.setPulledVersion(lastPulledVersion + 1);
-
-                System.out.println(availablePharmacy);
-                availablePharmacyRepository.save(availablePharmacy);
-            }
-        }
-        return "Operation Completed!";
-    }
+    // commented to not be triggered second time
+//    @GetMapping("/get-available-pharmacies")
+//    public String getAvailablePharmacies() {
+//        var daysFromToday = 0;
+//        var date = DateUtils.dateToString(DateUtils.getDateFromTodayPlusDays(daysFromToday));
+//        var workingHoursIdByPharmacyId = AvailablePharmacies.getAvailablePharmacyIdsAndWorkingHourIds(daysFromToday);
+//        AvailablePharmacy availablePharmacy;
+//
+//
+//        var tempAvailablePharmacy =
+//                (AvailablePharmacy) availablePharmacyRepository.getLastPulledVersion(date).toArray()[0];
+//        var lastPulledVersion = tempAvailablePharmacy.getPulledVersion();
+//
+//        if (workingHoursIdByPharmacyId != null) {
+//            for (var pair : workingHoursIdByPharmacyId.keySet()) {
+//                int pharmacyId = pair;
+//                int workingHourId = workingHoursIdByPharmacyId.get(pair);
+//
+//                availablePharmacy = new AvailablePharmacy();
+//                availablePharmacy.setId(0);
+//                availablePharmacy.setPharmacyId(pharmacyId);
+//                availablePharmacy.setWorkingHourId(workingHourId);
+//                availablePharmacy.setDate(date);
+//                availablePharmacy.setPulledVersion(lastPulledVersion + 1);
+//
+//                System.out.println(availablePharmacy);
+//                availablePharmacyRepository.save(availablePharmacy);
+//            }
+//        }
+//        return "Operation Completed!";
+//    }
 
     // commented to not be triggered second time
 //    @GetMapping("/get-pharmacies-info")
