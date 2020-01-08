4d7469480fb9ea599efcb14214a00bad0188cec9
DsTyM <stavrosyomafias@gmail.com>
2020-01-08 03:58:52 +0200

    Scheduler.java code refactored

--- src/main/java/com/dstym/pharmaciesondutyattica/scheduler/Scheduler.java
+++ src/main/java/com/dstym/pharmaciesondutyattica/scheduler/Scheduler.java
@@ -1,6 +1,10 @@
 package com.dstym.pharmaciesondutyattica.scheduler;
 
+import com.dstym.pharmaciesondutyattica.scraper.AvailablePharmacyScraper;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @EnableScheduling
@@ -9,18 +13,18 @@ public class Scheduler {
     // cron properties
     // <minute> <hour> <day-of-month> <month> <day-of-week> <year>
 
-//    @Scheduled(cron = "0 5/12 * * * *")
-//    public void getAvailablePharmaciesTwicePerDay() {
-//        var daysFromToday = 0;
-//        AvailablePharmacyScraper.saveAvailablePharmacies(daysFromToday);
-//    }
+    @Scheduled(cron = "0 5/12 * * * *")
+    public void getAvailablePharmaciesTwicePerDay() {
+        var daysFromToday = 0;
+        AvailablePharmacyScraper.saveAvailablePharmacies(daysFromToday);
+    }
 
     // run only once after startup
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void getAvailablePharmaciesAfterStartup() {
-//        var daysFromToday = 0;
-//        AvailablePharmacyScraper.saveAvailablePharmacies(daysFromToday);
-//    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void getAvailablePharmaciesAfterStartup() {
+        var daysFromToday = 0;
+        AvailablePharmacyScraper.saveAvailablePharmacies(daysFromToday);
+    }
 
 //    @EventListener(ApplicationReadyEvent.class)
 //    public void getAvailablePharmaciesForLastDaysAfterStartup() {
