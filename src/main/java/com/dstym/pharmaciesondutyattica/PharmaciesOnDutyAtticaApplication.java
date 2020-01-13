f9b9a72b149d8ac08ec368c586ab95295b2da978
DsTyM <stavrosyomafias@gmail.com>
2020-01-14 01:28:25 +0200

    swagger ui added for api documentation

--- src/main/java/com/dstym/pharmaciesondutyattica/PharmaciesOnDutyAtticaApplication.java
+++ src/main/java/com/dstym/pharmaciesondutyattica/PharmaciesOnDutyAtticaApplication.java
@@ -2,12 +2,12 @@ package com.dstym.pharmaciesondutyattica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PharmaciesOnDutyAtticaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PharmaciesOnDutyAtticaApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/dstym/pharmaciesondutyattica/config/DefaultViewConfig.java
@@ -0,0 +1,13 @@
+package com.dstym.pharmaciesondutyattica.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DefaultViewConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+    }
+}
