3d6aaaec8f82ccf138ca0397c9748cf337889c8d
DsTyM <stavrosyomafias@gmail.com>
2020-01-15 21:43:47 +0200

    DateUtilsTests added

---
+++ src/test/java/com/dstym/pharmaciesondutyattica/util/DateUtilsTest.java
@@ -0,0 +1,45 @@
+package com.dstym.pharmaciesondutyattica.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateUtilsTest {
+    @Test
+    void testDateToString_nonValidDate() {
+        var testStringDate = "some not valid string";
+        assertThrows(ParseException.class, () -> DateUtils.dateToString(
+                new SimpleDateFormat("d/M/yyyy").parse(testStringDate))
+        );
+    }
+
+    @Test
+    void testDateToString_validDate() throws ParseException {
+        var expectedDate = "8/1/2020";
+        var actualDate = DateUtils.dateToString(new SimpleDateFormat("d/M/yyyy").parse(expectedDate));
+        assertEquals(expectedDate, actualDate);
+    }
+
+    @Test
+    void testStringToDate_nonValidDate() {
+        var testStringDate = "some not valid string";
+
+        var expected = DateUtils.stringToDate(testStringDate);
+
+        // if an exception is cached, then stringToDate() will return null
+        // so we just assure that it is null
+        assertNull(expected);
+    }
+
+    @Test
+    void testStringToDate_validDate() throws ParseException {
+        var testStringDate = "8/1/2020";
+
+        var expectedDate = new SimpleDateFormat("d/M/yyyy").parse(testStringDate);
+        var actualDate = new SimpleDateFormat("d/M/yyyy").parse("8/1/2020");
+        assertEquals(expectedDate, actualDate);
+    }
+}
