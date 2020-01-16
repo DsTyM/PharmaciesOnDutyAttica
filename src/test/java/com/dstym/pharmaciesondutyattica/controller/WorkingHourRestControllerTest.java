9400b3cb2fe229b3bcad337d5d61ff0358022fb2
DsTyM <stavrosyomafias@gmail.com>
2020-01-16 21:26:22 +0200

    WorkingHourServiceTest.java and WorkingHourRestControllerTest.java created

---
+++ src/test/java/com/dstym/pharmaciesondutyattica/controller/WorkingHourRestControllerTest.java
@@ -0,0 +1,7 @@
+package com.dstym.pharmaciesondutyattica.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkingHourRestControllerTest {
+
+}
---
+++ src/test/java/com/dstym/pharmaciesondutyattica/service/WorkingHourServiceTest.java
@@ -0,0 +1,7 @@
+package com.dstym.pharmaciesondutyattica.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkingHourServiceTest {
+
+}
