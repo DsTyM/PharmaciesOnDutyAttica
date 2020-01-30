fc0796d15dad29c5d0fceee0e1d7bdfdae0ce019
DsTyM <stavrosyomafias@gmail.com>
2020-01-30 16:44:02 +0200

    angular: router and available-pharmacy component created

--- angular/pharmacies-on-duty-attica-front-end/src/app/app.module.ts
+++ angular/pharmacies-on-duty-attica-front-end/src/app/app.module.ts
@@ -2,17 +2,34 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SpinnerComponent } from './spinner/spinner.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {SpinnerComponent} from './spinner/spinner.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AvailablePharmacyComponent} from './available-pharmacy/available-pharmacy.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'available-pharmacies',
+    component: AvailablePharmacyComponent,
+    data: {title: 'Pharmacies On Duty'}
+  },
+  {
+    path: '',
+    redirectTo: '/available-pharmacies',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    AvailablePharmacyComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/pharmacies-on-duty-attica-front-end/src/app/available-pharmacy/available-pharmacy.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-available-pharmacy',
+  templateUrl: './available-pharmacy.component.html',
+  styleUrls: ['./available-pharmacy.component.css']
+})
+export class AvailablePharmacyComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
