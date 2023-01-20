8b4bd35dce6cf312a8642105b433acf25098b961
DsTyM <svolak97@gmail.com>
2023-01-20 08:26:29 +0200

    update to spring boot 3

--- src/main/java/com/dstym/pharmaciesondutyattica/config/OpenApiConfig.java
+++ src/main/java/com/dstym/pharmaciesondutyattica/config/OpenApiConfig.java
@@ -2,20 +2,11 @@ package com.dstym.pharmaciesondutyattica.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("api")
-                .pathsToMatch("/api/**")
-                .build();
-    }
-
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
